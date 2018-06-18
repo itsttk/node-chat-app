a0300e599e067b5cceaf3327378cccad611c4937
itsttk <tkt160230@utdallas.edu>
2018-06-18 11:53:51 -0400

    added connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT ||3000;
@@ -10,12 +12,24 @@ const port = process.env.PORT ||3000;
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
 
 
-app.listen(port,()=>{
+io.on('connection', (socket)=>{
+    console.log('new user connected');
+
+ 		socket.on('disconnect',()=>{
+			console.log('user was disconneceted');
+		});
+});
+
+
+
+server.listen(port,()=>{
 	console.log("server is up and running on", port);
 });
 
